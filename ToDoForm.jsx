642b20702d58dfda7fa1a5b474597af894897322
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-10-18 19:42:06 -0600

    Feat: Imported both components to app.jsx and moved styles to each component

--- ToDoForm.jsx
+++ ToDoForm.jsx
@@ -1,5 +1,32 @@
-import {Button, TextInput, View} from "react-native";
-
+import {Button, StyleSheet, TextInput, View} from "react-native";
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  completed: {
+    backgroundColor: '#e0e0e0',
+  },
+  taskText: {
+    fontSize: 16,
+  },
+  form: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+});
 
 export default function ToDoForm() {
   return (
@@ -11,4 +38,6 @@ export default function ToDoForm() {
       <Button title="Add" />
     </View>
   );
+
+
 }
--- ToDoList.jsx
+++ ToDoList.jsx
@@ -1,6 +1,35 @@
-import {Pressable, ScrollView, Text, View} from "react-native";
+import {Pressable, ScrollView, StyleSheet, Text, View} from "react-native";
+
 
 export default function ToDoList() {
+  const styles = StyleSheet.create({
+    task: {
+      padding: 10,
+      borderBottomWidth: 1,
+      borderColor: '#ccc',
+    },
+    completed: {
+      backgroundColor: '#e0e0e0',
+    },
+    taskText: {
+      fontSize: 16,
+    },
+    form: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginHorizontal: 20,
+      marginTop: 20,
+    },
+    input: {
+      flex: 1,
+      borderWidth: 1,
+      borderColor: '#ccc',
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      marginRight: 10,
+    },
+  });
   return (
     <ScrollView>
       <Pressable>
